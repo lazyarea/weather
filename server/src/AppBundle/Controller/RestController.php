b14b38f02ece45ae16e35f91cb2375416bfed01c
Your Name <you@example.com>
2016-05-07 07:33:52 -0400

    init server

---
+++ server/src/AppBundle/Controller/RestController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class RestController extends Controller
+{
+    /**
+     * @Route("/rest/", name="restpage")
+     */
+    public function indexAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
+        ]);
+    }
+}
